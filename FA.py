abfbc5ceba3d1e41af2c3435e154b33c5e8379e1
BenHuynh19 <39694671+BenHuynh19@users.noreply.github.com>
2023-05-30 13:35:13 +0200

    Add files via upload

---
+++ FA.py
@@ -0,0 +1,133 @@
+from sklearn import metrics, ensemble, preprocessing
+from sklearn.ensemble import _forest as forest
+from sklearn.tree import export_graphviz
+from sklearn_pandas import DataFrameMapper
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.impute import SimpleImputer as Imputer
+from sklearn.ensemble import RandomForestRegressor
+import PIL, os, numpy as np, math, collections, threading, json, bcolz, random, scipy, cv2
+
+from pandas.api.types import is_string_dtype, is_numeric_dtype, is_categorical_dtype
+import pandas as pd, pickle, sys, itertools, string, sys, re, datetime, time, shutil, copy
+from pandas_summary import DataFrameSummary
+from IPython.display import HTML, display
+import IPython, graphviz, sklearn_pandas, sklearn, warnings, pdb
+
+from matplotlib import pyplot as plt, rcParams, animation
+from ipywidgets import interact, interactive, fixed, widgets
+import seaborn as sns, matplotlib
+matplotlib.rc('animation', html='html5')
+import numpy as np
+np.set_printoptions(precision=5, linewidth=110, suppress=True)
+
+
+def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None,
+            preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+    """ proc_df takes a data frame df and splits off the response variable, and
+    changes the df into an entirely numeric dataframe. For each column of df 
+    which is not in skip_flds nor in ignore_flds, na values are replaced by the
+    median value of the column.
+
+    """
+    if not ignore_flds: ignore_flds=[]
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    else: df = df.copy()
+    ignored_flds = df.loc[:, ignore_flds]
+    df.drop(ignore_flds, axis=1, inplace=True)
+    if preproc_fn: preproc_fn(df)
+    
+    if y_fld is None: y = None #splitting of y_flds
+    else:
+        if not is_numeric_dtype(df[y_fld]): df[y_fld] = pd.Categorical(df[y_fld]).codes
+        y = df[y_fld].values
+        skip_flds += [y_fld]
+    df.drop(skip_flds, axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    else: na_dict = na_dict.copy()
+    na_dict_initial = na_dict.copy()
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    #df = pd.get_dummies(df, dummy_na=True)
+    df = pd.concat([ignored_flds, df], axis=1)
+    res = [df, y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
+
+
+def fix_missing(df, col, name, na_dict):
+    """ Fill missing data in a column of df with the median, and add a {name}_na column
+    which specifies if the data was missing.
+
+    """
+    if is_numeric_dtype(col):
+        if pd.isnull(col).sum() or (name in na_dict):
+            df[name+'_na'] = pd.isnull(col)
+            filler = na_dict[name] if name in na_dict else col.median()
+            df[name] = col.fillna(filler)
+            na_dict[name] = filler
+    return na_dict
+
+
+def scale_vars(df, mapper):
+    warnings.filterwarnings('ignore', category=sklearn.exceptions.DataConversionWarning)
+    if mapper is None:
+        map_f = [([n],StandardScaler()) for n in df.columns if is_numeric_dtype(df[n])]
+        mapper = DataFrameMapper(map_f).fit(df)
+    df[mapper.transformed_names_] = mapper.transform(df)
+    return mapper
+
+
+def numericalize(df, col, name, max_n_cat):
+    """ Changes the column col from a categorical type to it's integer codes.
+
+    """
+    if not is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = pd.Categorical(col).codes+1
+
+
+def rf_feat_importance(m, df):
+    return pd.DataFrame({'cols':df.columns, 'imp':m.feature_importances_}
+                       ).sort_values('imp', ascending=False)
+
+
+def plot_fi(fi): 
+    ax = fi.plot('full_description', 'imp', kind='barh', figsize=(12, 8), legend=False)
+    ax.set_xlabel('Important levels')
+    ax.set_ylabel('Features')
+    ax.set_title('Feature Importances')
+    plt.tight_layout()
+    plt.savefig('fi_imp.png')
+    plt.show()
+
+
+def get_sample(df,n):
+    """ Gets a random sample of n rows from df, without replacement.
+    """
+    idxs = sorted(np.random.permutation(len(df))[:n])
+    return df.iloc[idxs].copy()
+
+
+def rf_feat_importance(m, df):
+    return pd.DataFrame({'cols':df.columns, 'imp':m.feature_importances_}
+                       ).sort_values('imp', ascending=False)
+
+def set_rf_samples(n):
+    """ Changes Scikit learn's random forests to give each tree a random sample of
+    n random rows.
+    """
+    forest._generate_sample_indices = (lambda rs, n_samples:
+        forest.check_random_state(rs).randint(0, n_samples, n))
+
+def reset_rf_samples():
+    """ Undoes the changes produced by set_rf_samples.
+    """
+    forest._generate_sample_indices = (lambda rs, n_samples:
+        forest.check_random_state(rs).randint(0, n_samples, n_samples))
+
+
+
